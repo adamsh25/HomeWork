9b2bc4f445df5c0f5c64d214abfb9ebb7955e744
adamsh25 <tso250@gmail.com>
2016-09-22 00:22:03 +0300

    Fix Structure

---
+++ Common/AcCommon/Event.cs
@@ -0,0 +1,41 @@
+﻿using AcXmlParser;
+using GoogleSearcher;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcCommon
+{
+
+    [ParseClass(NodeName = "Event")]
+    public class Event : ISearchWithGoogle, IGotGoogleData
+    {
+        [ParseClass(NodeName = "System")]
+        public System System { get; set; }
+
+        [ParseClass(NodeName = "EventData")]
+        public EventData Data { get; set; }
+
+        public override string ToString()
+        {
+            return this.System.EventID.ToString();
+        }
+
+        public string TextToSearch
+        {
+            get
+            {
+                return this.System.EventID.ToString();
+            }
+            set { }
+        }
+
+        public string GoogleDataValue
+        {
+            get;
+            set;
+        }
+    }
+}
