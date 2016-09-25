90e1c430aa5c26aa0778b953eec03b2caf8e57d3
adamsh25 <tso250@gmail.com>
2016-09-25 23:18:21 +0300

    Fix Directory

---
+++ Common/GoogleSearcher/GoogleAPINotSupportedEventArgs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleSearcher
+{
+    public class GoogleAPINotSupportedEventArgs : EventArgs
+    {
+        public string ErrorMessage { get; set; }
+
+        public DateTime DateReported { get; set; }
+
+        public GoogleAPINotSupportedEventArgs(string errorMessage, DateTime dateReported)
+        {
+            this.ErrorMessage = errorMessage;
+            this.DateReported = dateReported;
+        }
+    }
+}
